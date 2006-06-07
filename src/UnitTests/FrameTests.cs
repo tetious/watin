591f792bf6cf8b7e730e51c48a3cc19ffbaf8f5d
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2006-06-07 12:59:24 +0000

    Added copyright notice and removed some redundant qualifiers (ReSharper is happy now).
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@74 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

--- src/UnitTests/FrameTests.cs
+++ src/UnitTests/FrameTests.cs
@@ -1,3 +1,22 @@
+#region WatiN Copyright (C) 2006 Jeroen van Menen
+
+// WatiN (Web Application Testing In dotNet)
+// Copyright (C) 2006 Jeroen van Menen
+//
+// This library is free software; you can redistribute it and/or modify it under the terms of the GNU 
+// Lesser General Public License as published by the Free Software Foundation; either version 2.1 of 
+// the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this library; 
+// if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+// 02111-1307 USA 
+
+#endregion Copyright
+
 using System;
 using System.Collections;
 using System.IO;
@@ -29,7 +48,7 @@ namespace WatiN.UnitTests
 
       Logger.LogWriter = new DebugLogWriter();
 
-      string htmlLocation = new DirectoryInfo(System.Environment.CurrentDirectory).Parent.Parent.FullName + @"\html\";
+      string htmlLocation = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.FullName + @"\html\";
             
       htmlTestBaseURI = new Uri(htmlLocation);
       indexURI = new Uri(htmlTestBaseURI, "Index.html");
@@ -105,7 +124,7 @@ namespace WatiN.UnitTests
     [Test]
     public void ShowFrames()
     {
-      WatiN.Core.UtilityClass.ShowFrames(ie.MainDocument);
+      UtilityClass.ShowFrames(ie.MainDocument);
     }
 
     private static void AssertFindFrame(IE ie, AttributeValue findBy, string expectedFrameName)
--- src/UnitTests/Utils.cs
+++ src/UnitTests/Utils.cs
@@ -1,3 +1,22 @@
+#region WatiN Copyright (C) 2006 Jeroen van Menen
+
+// WatiN (Web Application Testing In dotNet)
+// Copyright (C) 2006 Jeroen van Menen
+//
+// This library is free software; you can redistribute it and/or modify it under the terms of the GNU 
+// Lesser General Public License as published by the Free Software Foundation; either version 2.1 of 
+// the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this library; 
+// if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+// 02111-1307 USA 
+
+#endregion Copyright
+
 using System;
 using System.IO;
 
@@ -17,7 +36,7 @@ namespace WatiN.UnitTests
     {
       System.Threading.Thread.CurrentThread.ApartmentState = System.Threading.ApartmentState.STA;
 
-      string htmlTestLocation = new DirectoryInfo(System.Environment.CurrentDirectory).Parent.Parent.FullName + @"\html\";
+      string htmlTestLocation = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.FullName + @"\html\";
             
       htmlTestBaseURI = new Uri(htmlTestLocation);
     }
@@ -27,7 +46,7 @@ namespace WatiN.UnitTests
     {
       using (IE ie = new IE(htmlTestBaseURI + "main.html"))
       {
-        WatiN.Core.UtilityClass.DumpElements(ie.MainDocument);
+        UtilityClass.DumpElements(ie.MainDocument);
       }
     }
 
@@ -36,7 +55,7 @@ namespace WatiN.UnitTests
     {
       using (IE ie = new IE(htmlTestBaseURI + "Frameset.html"))
       {
-        WatiN.Core.UtilityClass.DumpElementsWithHtmlSource(ie.MainDocument);
+        UtilityClass.DumpElementsWithHtmlSource(ie.MainDocument);
       }
     }
   }
