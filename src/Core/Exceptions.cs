c0fd74cf0e1e801103eb4f78875709f146a9d582
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2006-08-23 15:31:19 +0000

    Corrected a typo in the documentation.
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@137 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

--- src/Core/Exceptions.cs
+++ src/Core/Exceptions.cs
@@ -101,7 +101,7 @@ namespace WatiN.Core.Exceptions
   }
 
   /// <summary>
-  /// Thrown if an element is readobly and the current action (like TextField.TypeText) a
+  /// Thrown if an element is readonly and the current action (like TextField.TypeText) a
   /// is not allowed.
   /// </summary>
   public class ElementReadOnlyException : WatiNException
