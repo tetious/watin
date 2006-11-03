4d133ddb8d3441bab65c5578e53809c18cec1e89
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2006-11-03 10:49:34 +0000

    Added basic support for handling the print dialog through PrintDialogHandler. Thanks to Marcel Veselka for donating this code!
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@269 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

--- src/Core/DialogWatcher.cs
+++ src/Core/DialogWatcher.cs
@@ -1014,4 +1014,71 @@ namespace WatiN.Core.DialogHandlers
 
     #endregion
   }
+  
+  /// <summary>
+  /// This class handles the print dialog clicking the Print, 
+  /// Cancel or Apply button.
+  /// </summary>
+  /// <example>
+  /// The following code shows the use of this dialog handler
+  /// <code>
+  /// IE ie = new IE();
+  ///
+  /// ie.DialogWatcher.Add(new PrintDialogHandler(PrintDialogHandler.ButtonsEnum.Cancel));
+  ///
+  /// ie.GoTo("http://www.someprintdialog.com");
+  /// </code>
+  /// </example>
+  public class PrintDialogHandler : BaseDialogHandler
+  {
+    public enum ButtonsEnum
+    {
+      Print = 1,
+      Cancel = 2,
+    }
+
+    // need to be checked whether this is valid for other operating 
+    // systems - chcecked on WinXP and Win2003
+    private const string printWarningDialogStyle = "96C820C4";
+
+    private ButtonsEnum buttonToPush;
+
+    public PrintDialogHandler(ButtonsEnum 
+      buttonToPush)
+    {
+      this.buttonToPush = buttonToPush;
+    }
+
+    public override bool HandleDialog(Window window)
+    {
+      if (IsPrintDialog(window))
+      {
+        NativeMethods.SetForegroundWindow(window.Hwnd);
+        NativeMethods.SetActiveWindow(window.Hwnd);
+
+        ButtonToPush(window).Click();
+        
+        return true;
+      }
+
+      return false;
+    }
+
+    private WinButton ButtonToPush(Window window)
+    {
+      return new WinButton((int)buttonToPush, window.Hwnd);
+    }
+
+    /// <summary>
+    /// Determines whether the specified window is a print dialog.
+    /// </summary>
+    /// <param name="window">The window.</param>
+    /// <returns>
+    /// <c>true</c> if the specified window is a print dialog; otherwise, <c>false</c>.
+    /// </returns>
+    public bool IsPrintDialog(Window window)
+    {
+      return (window.StyleInHex == printWarningDialogStyle);
+    }
+  }
 }
