72d458f992ba0bbb2fdb4fa5305ee5878b475f71
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2007-02-07 14:26:12 +0000

    Added SecurityAlertDialogHandler
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@352 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

--- src/Core/DialogWatcher.cs
+++ src/Core/DialogWatcher.cs
@@ -722,6 +722,50 @@ namespace WatiN.Core.DialogHandlers
     }
   }
   
+  /// <summary>
+  /// This class handles the Security Warning dialog and does press
+  /// the OK button when the dialog shows up.
+  /// </summary>
+  public class SecurityAlertDialogHandler : IDialogHandler
+  {
+    private const string securityAlertDialogStyle = "94C80AC4";
+
+    /// <summary>
+    /// Handles the dialog if the <paramref name="window" /> is a
+    /// security alert dialog.
+    /// </summary>
+    /// <param name="window">The window.</param>
+    /// <returns></returns>
+    public bool HandleDialog(Window window)
+    {
+      if (IsSecurityAlertDialog(window))
+      {
+        NativeMethods.SetForegroundWindow(window.Hwnd);
+        NativeMethods.SetActiveWindow(window.Hwnd);
+
+        WinButton buttonOk = new WinButton(1, window.Hwnd);
+        buttonOk.Click();
+      
+        return true;
+      }
+      
+      return false;
+    }
+
+    /// <summary>
+    /// Determines whether the specified window is a security alert dialog.
+    /// </summary>
+    /// <param name="window">The window.</param>
+    /// <returns>
+    /// 	<c>true</c> if the specified window is a security alert dialog; otherwise, <c>false</c>.
+    /// </returns>
+    protected virtual bool IsSecurityAlertDialog(Window window)
+    {     
+      return window.StyleInHex == securityAlertDialogStyle;
+    }
+
+  }
+
   public class FileUploadDialogHandler : BaseDialogHandler
   {
     private String fileName;
