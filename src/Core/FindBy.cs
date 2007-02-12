eca7fdd4f03433741e0db81a451ee608d799a7fe
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2007-02-12 17:06:24 +0000

    - Added documentation to the new methods of the Attribute class - made doCompare, LockCompare and UnLockCompare protected virtual
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@360 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

--- src/Core/FindBy.cs
+++ src/Core/FindBy.cs
@@ -316,10 +316,40 @@ namespace WatiN.Core
     /// <summary>
     /// This methode implements an exact match comparison. If you want
     /// different behaviour, inherit this class or one of its subclasses and 
-    /// override Compare with a specific implementation.
+    /// override Compare with a specific implementation. 
+    /// <seealso cref="LockCompare"/>
+    /// <seealso cref="UnLockCompare"/>
     /// </summary>
     /// <param name="attributeBag">Value to compare with</param>
     /// <returns><c>true</c> if the searched for value equals the given value</returns>
+    /// <example>
+    /// The following example shows the use of the LockCompare and UnlockCompare methods.
+    /// <code>
+    ///   public override bool Compare(IAttributeBag attributeBag)
+    ///    {
+    ///      bool result = false;
+    /// 
+    ///      // Call LockCompare if you don't call base.compare.
+    ///      base.LockCompare();
+    ///
+    ///      try
+    ///      {
+    ///         // Your compare code goes here.
+    /// 
+    ///         // Don't call base.compare here because that will throw
+    ///         // a ReEntryException since you already locked the compare
+    ///         // method for recursive calls.
+    ///      }
+    ///      finally
+    ///      {
+    ///      // Call UnLockCompare if you called LockCompare.
+    ///        base.UnLockCompare();
+    ///      }      
+    ///
+    ///      return result;
+    ///    }
+    /// </code>
+    /// </example>
     public virtual bool Compare(IAttributeBag attributeBag)
     {
       LockCompare();
@@ -337,7 +367,11 @@ namespace WatiN.Core
       return returnValue;
     }
 
-    private bool doCompare(IAttributeBag attributeBag)
+    /// <summary>
+    /// Does the compare without calling <see cref="LockCompare"/> and <see cref="UnLockCompare"/>.
+    /// </summary>
+    /// <param name="attributeBag">The attribute bag.</param>
+    protected virtual bool doCompare(IAttributeBag attributeBag)
     {
       bool returnValue = comparer.Compare(attributeBag.GetValue(attributeName));
       
@@ -353,12 +387,53 @@ namespace WatiN.Core
       return returnValue;
     }
 
-    private void UnLockCompare()
+
+    /// <summary>
+    /// Call this method to unlock the compare method. Typically done at the
+    /// end of your compare method. 
+    /// <seealso cref="LockCompare"/>
+    /// <seealso cref="Compare"/>
+    /// </summary>
+    protected virtual void UnLockCompare()
     {
       busyComparing = false;
     }
 
-    private void LockCompare()
+    /// <summary>
+    /// Call this method if you override the Compare method and don't call base.compare.
+    /// You should typically call this method as the first line of code in your
+    /// compare method. Calling this will prevent unnoticed reentry problems, resulting
+    /// in a StackOverflowException when your attribute class is recursively used in a multiple
+    /// findby scenario.
+    /// <seealso cref="Compare"/>
+    /// <seealso cref="UnLockCompare"/>
+    /// </summary>
+    /// <example>
+    /// The following example shows the use of the LockCompare and UnlockCompare methods.
+    /// <code>
+    ///   public override bool Compare(IAttributeBag attributeBag)
+    ///    {
+    ///      bool result = false;
+    /// 
+    ///      base.LockCompare();
+    ///
+    ///      try
+    ///      {
+    ///         // Your compare code goes here.
+    ///         // Don't call base.compare here because that will throw
+    ///         // a ReEntryException since you already locked the compare
+    ///         // method for recursive calls.
+    ///      }
+    ///      finally
+    ///      {
+    ///        base.UnLockCompare();
+    ///      }      
+    ///
+    ///      return result;
+    ///    }
+    /// </code>
+    /// </example>
+    protected virtual void LockCompare()
     {
       if (busyComparing)
       {
@@ -369,6 +444,30 @@ namespace WatiN.Core
       busyComparing = true;
     }
 
+    /// <summary>
+    /// Adds the specified attribute to the And Attribute chain of a multiple <see cref="Attribute"/>
+    /// element search. When calling And or using the operators, WatiN will always use
+    /// ConditionAnd (&amp;&amp;) during the evaluation.
+    /// <seealso cref="Or"/>
+    /// </summary>
+    /// <param name="attribute">The <see cref="Attribute"/> instance.</param>
+    /// <returns>This <see cref="Attribute"/></returns>
+    /// <example>
+    /// If you want to find a Button by it's name and value this example shows you how to use
+    /// the And method to do this:
+    /// <code> 
+    /// IE ie = new IE("www.yourwebsite.com/yourpage.htm");
+    /// 
+    /// Button myButton = ie.Button(Find.ByName("buttonname").And(Find.ByValue("Button value")));
+    /// </code>
+    /// 
+    /// You can also use the &amp; or &amp;&amp; operators, resulting in a bit more readable code.
+    /// <code> 
+    /// IE ie = new IE("www.yourwebsite.com/yourpage.htm");
+    /// 
+    /// Button myButton = ie.Button(Find.ByName("buttonname") &amp; Find.ByValue("Button value"));
+    /// </code>
+    /// </example>
     public Attribute And(Attribute attribute)
     {
       if (andAttribute == null)
@@ -385,6 +484,30 @@ namespace WatiN.Core
       return this;
     }
     
+    /// <summary>
+    /// Adds the specified attribute to the Or Attribute chain of a multiple <see cref="Attribute"/>
+    /// element search. When calling Or or using the | or || operators, WatiN will always use
+    /// ConditionOr (||) during the evaluation.
+    /// <seealso cref="And"/>
+    /// </summary>
+    /// <param name="attribute">The <see cref="Attribute"/> instance.</param>
+    /// <returns>This <see cref="Attribute"/></returns>
+    /// <example>
+    /// If you want to find a Button by it's English or Dutch value this example shows you how to use
+    /// the Or method to do this:
+    /// <code> 
+    /// IE ie = new IE("www.yourwebsite.com/yourpage.htm");
+    /// 
+    /// Button myButton = ie.Button(Find.ByValue("Cancel").Or(Find.ByValue("Annuleren")));
+    /// </code>
+    /// 
+    /// You can also use the | or || operators, resulting in a bit more readable code.
+    /// <code> 
+    /// IE ie = new IE("www.yourwebsite.com/yourpage.htm");
+    /// 
+    /// Button myButton = ie.Button(Find.ByValue("Cancel") || Find.ByValue("Annuleren"));
+    /// </code>
+    /// </example>
     public Attribute Or(Attribute attribute)
     {
       if (orAttribute == null)
@@ -402,6 +525,12 @@ namespace WatiN.Core
       return this;
     }
 
+    /// <summary>
+    /// Returns a <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+    /// </summary>
+    /// <returns>
+    /// The Value of this <see cref="Attribute" />
+    /// </returns>
     public override string ToString()
     {
       return Value;
@@ -503,24 +632,35 @@ namespace WatiN.Core
     
     public override bool Compare(IAttributeBag attributeBag)
     {
-      bool resultAnd = false;
-      bool resultOr = false;
-      
-      if (andAttribute != null)
-      {
-        resultAnd = andAttribute.Compare(attributeBag);
-      }
+      base.LockCompare();
 
-      if (resultAnd || andAttribute == null)
+      bool resultOr;
+
+      try
       {
-        counter++;
-      }
+        bool resultAnd = false;
+        resultOr = false;
       
-      if (orAttribute != null && resultAnd == false)
+        if (andAttribute != null)
+        {
+          resultAnd = andAttribute.Compare(attributeBag);
+        }
+
+        if (resultAnd || andAttribute == null)
+        {
+          counter++;
+        }
+      
+        if (orAttribute != null && resultAnd == false)
+        {
+          resultOr = orAttribute.Compare(attributeBag);
+        }
+      }
+      finally
       {
-        resultOr = orAttribute.Compare(attributeBag);
-      }
-      
+        base.UnLockCompare();
+      }      
+
       return (counter == occurrence) || resultOr;
     }
   }
@@ -625,6 +765,7 @@ namespace WatiN.Core
     /// <summary>
     /// Initializes a new instance of the <see cref="StyleAttribute"/> class.
     /// </summary>
+    /// <param name="styleAttributeName">Name of the style attribute.</param>
     /// <param name="comparer">The comparer.</param>
     public StyleAttribute(string styleAttributeName, ICompare comparer) : base(attributeName + styleAttributeName, comparer)
     {}
