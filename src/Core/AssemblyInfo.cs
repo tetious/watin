05d9845dd10b96cd695ee4bb3a8ff447964115fa
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2007-04-08 21:08:08 +0000

    Updated versionnumber
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@411 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

--- src/Core/AssemblyInfo.cs
+++ src/Core/AssemblyInfo.cs
@@ -43,7 +43,7 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.1.0.1000")]
+[assembly: AssemblyVersion("1.1.0.1001")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
