95c9ce9d0fe4ca56ee98bb8a39832abd04dc40f3
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2008-04-01 21:10:11 +0000

    - Added PromptDialogHandler + "manual" test - Added App.config so tests can run using ReSharper 4.0 - cleaned up TestEvents.html
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@624 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

---
+++ src/Core/DialogHandlers/PromptDialogHandler.cs
@@ -0,0 +1,80 @@
+﻿#region WatiN Copyright (C) 2006-2008 Jeroen van Menen
+
+//Copyright 2006-2008 Jeroen van Menen
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+#endregion Copyright
+
+using System;
+
+namespace WatiN.Core.DialogHandlers
+{
+    public class PromptDialogHandler : BaseDialogHandler
+    {
+        readonly string _input;
+        readonly bool _cancel;
+
+        public PromptDialogHandler(bool cancel)
+        {
+            _cancel = cancel;
+        }
+
+        public PromptDialogHandler(string input)
+        {
+            _input = input;
+            _cancel = false;
+        }
+
+        public override bool HandleDialog(Window window)
+        {
+            if (IsPromptDialog(window))
+            {
+                window.ToFront();
+                window.SetActivate();
+
+                Window inputBox = new
+                Window(NativeMethods.GetChildWindowHwnd(window.Hwnd, "Edit"));
+
+                if (inputBox.Hwnd != IntPtr.Zero)
+                {
+                    if (_cancel)
+                    {
+                        window.ForceClose();
+                    }
+                    else
+                    {
+                        NativeMethods.SetActiveWindow(inputBox.Hwnd);
+
+                        System.Windows.Forms.SendKeys.SendWait(_input);
+                        System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the window is a prompt dialog.
+        /// </summary>
+        /// <param name="window">The window.</param>
+        /// <returns>
+        /// 	<c>true</c> if window is a prompt dialog; otherwise, <c>false</c>.
+        /// </returns>
+        public bool IsPromptDialog(Window window)
+        {
+            return (window.StyleInHex == "94C800C4");
+        }
+    }
+}
---
+++ src/UnitTests/DialogHandlerTests/PromptDialogHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using WatiN.Core.DialogHandlers;
+
+namespace WatiN.Core.UnitTests.DialogHandlerTests
+{
+    [TestFixture]
+    public class PromptDialogHandlerTests
+    {
+        /// <summary>
+        /// Because of popup blockers this test will almost always fail. 
+        /// To run the test succesfully: 1 - Open the TestEvents.html page in IE.
+        /// 2 - Click on the Show prompt button
+        /// 3 - Handle the warning on top of the IE window, select to temporarily allow the scripts to run on this page
+        /// 4 - Now you can start this unit test (remove Ignore attribute first)
+        /// </summary>
+        [Test, Ignore("IE popup blocker will cause this test to fail. Read method documentation to run this test manually")]
+        public void PromptDialogHandler()
+        {
+            IE ie = IE.AttachToIE(Find.ByUrl(new Regex("TestEvents.html$")));
+
+            PromptDialogHandler handler = new PromptDialogHandler("Hello");
+            using (new UseDialogOnce(ie.DialogWatcher, handler))
+            {
+                ie.Button("showPrompt").Click();
+            }
+            Assert.That(ie.TextField("promptResult").Value, Is.EqualTo("Hello"), "input did not work. IE might be blocking the prompt dialog");
+        }
+    }
+}
