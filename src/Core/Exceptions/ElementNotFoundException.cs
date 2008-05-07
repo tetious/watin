4884410830c83d8748f2b9a50390704d89951061
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2008-05-07 15:03:53 +0000

    Marked constructors Obsolete
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@641 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

--- src/Core/Exceptions/ElementNotFoundException.cs
+++ src/Core/Exceptions/ElementNotFoundException.cs
@@ -25,13 +25,15 @@ namespace WatiN.Core.Exceptions
 	/// </summary>
 	public class ElementNotFoundException : WatiNException
 	{
+        [Obsolete("Use new constructor which also accepts a url")]
 		public ElementNotFoundException(string tagName, string criteria) :
 			base(CreateMessage(tagName, criteria, null, null)) {}
 
 		public ElementNotFoundException(string tagName, string criteria, string url) :
 			base(CreateMessage(tagName, criteria, url, null)) {}
 
-		public ElementNotFoundException(string tagName, string criteria, Exception innerexception) :
+        [Obsolete("Use new constructor which also accepts a url")]
+        public ElementNotFoundException(string tagName, string criteria, Exception innerexception) :
 			base(CreateMessage(tagName, criteria, null, innerexception.Message), innerexception) {}
 
 		public ElementNotFoundException(string tagName, string criteria, string url, Exception innerexception) :
