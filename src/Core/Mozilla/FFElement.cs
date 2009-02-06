74a16181b2faffd24bcda87cfeaeebb75136c566
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2009-02-06 22:55:32 +0000

    More copyright updates: changed 2008 to 2009
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@778 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

--- src/Core/Mozilla/FFElement.cs
+++ src/Core/Mozilla/FFElement.cs
@@ -1,6 +1,6 @@
 ﻿#region WatiN Copyright (C) 2006-2009 Jeroen van Menen
 
-//Copyright 2006-2008 Jeroen van Menen
+//Copyright 2006-2009 Jeroen van Menen
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
