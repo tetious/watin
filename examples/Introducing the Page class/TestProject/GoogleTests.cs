0ef49685b66712243ea0d957b1321a5936c9fe8c
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2009-06-17 16:00:51 +0000

    New example project showing the use of the Page class and PageAttribute
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@967 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

---
+++ examples/Introducing the Page class/TestProject/GoogleTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WatiN.Core;
+
+namespace TestProject
+{
+    [TestClass]
+    public class GoogleTests
+    {
+        [TestMethod]
+        public void Search_for_watin_on_google_the_old_way()
+        {
+            using (var browser = new IE("http://www.google.com"))
+            {
+                browser.TextField(Find.ByName("q")).TypeText("WatiN");
+                browser.Button(Find.ByName("btnG")).Click();
+
+                Assert.IsTrue(browser.ContainsText("WatiN"));
+            }
+        }
+
+        [TestMethod]
+        public void Search_for_watin_on_google_using_page_class()
+        {
+            using (var browser = new IE("http://www.google.com"))
+            {
+                var searchPage = browser.Page<GoogleSearchPage>();
+                searchPage.SearchCriteria.TypeText("WatiN");
+                searchPage.SearchButton.Click();
+
+                Assert.IsTrue(browser.ContainsText("WatiN"));
+            }
+        }
+
+        [TestMethod]
+        public void Page_with_an_action()
+        {
+            using (var browser = new IE("http://www.google.com"))
+            {
+                browser.Page<GoogleSearchPage>().SearchFor("WatiN");
+
+                Assert.IsTrue(browser.ContainsText("WatiN"));
+            }
+        }
+
+        [Page(UrlRegex = "www.google.*")]
+        public class GoogleSearchPage : Page
+        {
+            public TextField SearchCriteria
+            {
+                get { return Document.TextField(Find.ByName("q")); }
+            }
+
+            public Button SearchButton
+            {
+                get { return Document.Button(Find.ByName("btnG")); }
+            }
+
+            public void SearchFor(string searchCriteria)
+            {
+                SearchCriteria.TypeText("WatiN");
+                SearchButton.Click();
+            }
+        }
+    }
+}
