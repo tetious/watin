02184736a67d8f6b12c643c90d83e1263f97a25b
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2009-12-07 16:35:44 +0000

    Work in progress: Example how to create a DatePickerControl for JQuery-UI datepicker widget
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@1081 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

---
+++ examples/Creating a DateTime control/TestProject/DatePickerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WatiN.Controls.JQuery.UI
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class DatePickerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic	here
+            //
+        }
+    }
+}
