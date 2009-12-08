b8d862042ad668a4e5945079001cbe1e415f4309
jvmenen <jvmenen@6ca19b5b-dac0-4595-9ef0-4637b6ca95b2>
2009-12-08 11:52:23 +0000

    Implemented setting and getting Date on DatePicker control
    git-svn-id: svn://svn.code.sf.net/p/watin/code/trunk@1085 6ca19b5b-dac0-4595-9ef0-4637b6ca95b2

--- examples/Creating a DateTime control/TestProject/DatePickerTests.cs
+++ examples/Creating a DateTime control/TestProject/DatePickerTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WatiN.Core;
 
 namespace WatiN.Controls.JQuery.UI
 {
@@ -9,11 +11,90 @@ namespace WatiN.Controls.JQuery.UI
     public class DatePickerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Should_set_date_in_date_field()
         {
-            //
-            // TODO: Add test logic	here
-            //
+            // GIVEN
+            using (var browser = new FireFox("http://jqueryui.com/demos/datepicker/"))
+            {
+                var datePicker = browser.Control<DatePicker>("datepicker");
+                
+                // WHEN
+                datePicker.Date = new DateTime(2009, 12, 6);
+
+                // THEN
+                Assert.AreEqual("12/06/2009", browser.TextField("datepicker").Text);
+            }
+        }
+
+        [TestMethod]
+        public void Should_get_the_date()
+        {
+            // GIVEN
+            using (var browser = new FireFox("http://jqueryui.com/demos/datepicker/"))
+            {
+                var datePicker = browser.Control<DatePicker>("datepicker");
+                datePicker.Date = new DateTime(2009, 12, 6);
+                
+                // WHEN
+                var date = datePicker.Date;
+
+                // THEN
+                Assert.AreEqual(new DateTime(2009, 12, 6), date);
+            }
+        }
+    }
+
+    public class DatePicker : Control<TextField>
+    {
+        public override Core.Constraints.Constraint ElementConstraint
+        {
+            get
+            {
+                return Find.ByClass("hasDatepicker");
+            }
+        }
+        public DateTime? Date
+        {
+            get
+            {
+                var date = Eval(string.Format("{0}.datepicker('getDate').toUTCString()", ElementReference));
+
+                return DateTime.Parse(date);
+            }
+            set
+            {
+                // TODO: implement clearing of Date
+                if (!value.HasValue) return;
+                
+                var date = value.Value;
+
+                var script = string.Format("{0}.datepicker('setDate', new Date({1}, {2}, {3}))", ElementReference, date.Year, date.Month - 1, date.Day);
+                RunScript(script);
+            }
+        }
+
+        public string DateFormat
+        {
+            get
+            {
+                var script = string.Format("{0}.datepicker('option', 'dateFormat')", ElementReference);
+                return Eval(script);
+            }
+        }
+
+        private string ElementReference
+        {
+            get { return string.Format("window.$('#{0}')", Element.Id) ;}
+        }
+
+        private string Eval(string script)
+        {
+            return Element.DomContainer.Eval(script);
+        }
+
+        private void RunScript(string script)
+        {
+            Element.DomContainer.RunScript(script);
         }
     }
 }
